2c83c4cfc819d58030089e87f9f7da17a37f9ea3
Kavin Abregana <abreganakavin@gmail.com>
2024-06-20 23:49:57 +0800

    update bgcolor and logo

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import UgcGallery from "./components/UgcGallery";
 function App() {
   //Instance ID
   let InstanceId_Widget = "590388";
-  let InstanceId_QA = "780645";
+  let InstanceId_QA = "";
   let InstanceId_SEO = "780647";
   let InstanceId_Carousel = "780648";
   let InstanceId_StarRating = "780644";
